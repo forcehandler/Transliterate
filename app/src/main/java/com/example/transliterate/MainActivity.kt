aaec620ba71fb57d4aafeea439747f94b315baf2
Abhiraj Saxena <sabhira@amazon.com>
2021-03-18 17:08:01 +0530

    First commit

---
+++ app/src/main/java/com/example/transliterate/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.transliterate
+
+import android.icu.text.Transliterator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var string = "My name is joker"
+
+    }
+}
